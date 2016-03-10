aac2ef7f7dceebfaea1a94d2d49f16760c4a01ea
Dementiev Nikolay <dementievnikolay@gmail.com>
2016-03-10 14:07:07 +0200

    CoreData test _1

--- SortList/SortList/SortList/AppDelegate.swift
+++ SortList/SortList/SortList/AppDelegate.swift
@@ -43,6 +43,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        self.saveContext()
     }
 
     // MARK: - Core Data stack
@@ -55,7 +56,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     lazy var managedObjectModel: NSManagedObjectModel = {
         // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
-        let modelURL = NSBundle.mainBundle().URLForResource("CoreDataTest", withExtension: "momd")!
+        let modelURL = NSBundle.mainBundle().URLForResource("CoreDataModel", withExtension: "momd")!
         return NSManagedObjectModel(contentsOfURL: modelURL)!
     }()
     
@@ -63,7 +64,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
         // Create the coordinator and store
         let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("SingleViewCoreData.sqlite")
+        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("SortList.sqlite")
         var failureReason = "There was an error creating or loading the application's saved data."
         do {
             try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
--- SortList/SortList/SortList/Controllers/ToDoListViewController.swift
+++ SortList/SortList/SortList/Controllers/ToDoListViewController.swift
@@ -7,10 +7,20 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoListViewController: UIViewController, ToDoListTableViewDelegate, DataEnteredDelegate,ToDoItemActionSheetControlDelegate {
     
-    var toDoItems: [ToDoItem] = []
+    var toDoItems: [ToDoItem] = [] {
+        didSet{
+            
+            
+        }
+    }
+    //var toDoItems: [NSManagedObject] = []
+    
+    var managedObjectContext: NSManagedObjectContext? = nil
+    
     //var newItem: String = ""
     //    var recivedFromMainListValue: String = ""
     var recivedFromMainListValueCell: ToDoItem? = nil
@@ -182,28 +192,103 @@ class ToDoListViewController: UIViewController, ToDoListTableViewDelegate, DataE
         NSUserDefaults.standardUserDefaults().synchronize()
     }
     
-    //MARK: ReloadDataInTableView from ActionSheetCellControls
-    
-    //    func ReloadDataInTableViewDidInn () {
-    //        toDoListTableView.reloadData()
-    //    }
-    
-    //    func ReloadDataInTableViewDid () {
-    //        delegateToReloadDataInTableViewDidInn?.ReloadDataInTableViewDidInn()
-    //    }
-    
-    /*
-    @IBAction func cancel(segue:UIStoryboardSegue) {
-    
+    //MARK: SAVE CoreData
+    func insertNewObject(sender: AnyObject) {
+        let context = self.fetchedResultsController.managedObjectContext
+        let entity = self.fetchedResultsController.fetchRequest.entity!
+        let newManagedObject = NSEntityDescription.insertNewObjectForEntityForName(entity.name!, inManagedObjectContext: context)
+        
+        // If appropriate, configure the new managed object.
+        // Normally you should use accessor methods, but using KVC here avoids the need to add a custom class to the template.
+        newManagedObject.setValue(NSDate(), forKey: "dateTimeCreate")
+        
+        // Save the context.
+        do {
+            try context.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            //print("Unresolved error \(error), \(error.userInfo)")
+            abort()
+        }
     }
     
-    @IBAction func done(segue:UIStoryboardSegue) {
-    let itemDetailVC = segue.sourceViewController as! DetailViewController
-    newItem = itemDetailVC.labelText
-    
-    toDoItems.append(ToDoItem.init(item: newItem, checked: false))
+    func configureCell(cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) {
+        //let object = self.fetchedResultsController.objectAtIndexPath(indexPath)
+        //cell.textLabel!.text = object.valueForKey("timeStamp")!.description
     }
-    */
+    
+    // MARK: - Fetched results controller
+    
+    var fetchedResultsController: NSFetchedResultsController {
+        if _fetchedResultsController != nil {
+            return _fetchedResultsController!
+        }
+        
+        let fetchRequest = NSFetchRequest()
+        // Edit the entity name as appropriate.
+        let entity = NSEntityDescription.entityForName("CoreToDoItems", inManagedObjectContext: self.managedObjectContext!)
+        fetchRequest.entity = entity
+        
+        // Set the batch size to a suitable number.
+        fetchRequest.fetchBatchSize = 20
+        
+        // Edit the sort key as appropriate.
+        let sortDescriptor = NSSortDescriptor(key: "item", ascending: false)
+        
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        // Edit the section name key path and cache name if appropriate.
+        // nil for section name key path means "no sections".
+        let aFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext!, sectionNameKeyPath: nil, cacheName: "Master")
+   //     aFetchedResultsController.delegate = self
+        _fetchedResultsController = aFetchedResultsController
+        
+        do {
+            try _fetchedResultsController!.performFetch()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            //print("Unresolved error \(error), \(error.userInfo)")
+            abort()
+        }
+        
+        return _fetchedResultsController!
+    }
+    var _fetchedResultsController: NSFetchedResultsController? = nil
+    
+    func controllerWillChangeContent(controller: NSFetchedResultsController) {
+        self.toDoListTableView.beginUpdates()
+    }
+    
+    func controller(controller: NSFetchedResultsController, didChangeSection sectionInfo: NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
+        switch type {
+        case .Insert:
+            self.toDoListTableView.insertSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
+        case .Delete:
+            self.toDoListTableView.deleteSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
+        default:
+            return
+        }
+    }
+    
+    func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
+        switch type {
+        case .Insert:
+            toDoListTableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
+        case .Delete:
+            toDoListTableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
+        case .Update:
+            self.configureCell(toDoListTableView.cellForRowAtIndexPath(indexPath!)!, atIndexPath: indexPath!)
+        case .Move:
+            toDoListTableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
+            toDoListTableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
+        }
+    }
+    
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        self.toDoListTableView.endUpdates()
+    }
     
 }
 
